03493dc6542286a524d0a177507a24057eda05f3
luispoicon1 <lpoicon0@gmail.com>
2024-11-21 18:26:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-function showDetails(title, description, price) {
+function showDetails(title, description, price, imageUrl) {
+    // Actualiza los detalles del producto en el modal
     document.getElementById('modal-title').innerText = title;
     document.getElementById('modal-description').innerText = description;
     document.getElementById('modal-price').innerText = price;
+
+    // Actualiza la imagen del producto
+    const modalImage = document.getElementById('modal-image');
+    modalImage.src = imageUrl;
+    modalImage.alt = `Imagen de ${title}`;
+
+    // Muestra el modal
     document.getElementById('product-modal').style.display = 'flex';
 }
 
 function closeModal() {
+    // Oculta el modal
     document.getElementById('product-modal').style.display = 'none';
 }
